7011101323e1012ae056b6b7e7683bc19182c090
DESKTOP-NUOPUIF\ITPS <fj2008@naver.com>
2021-03-18 14:41:06 +0900

    d

---
+++ src/chapter2/Ex5.java
@@ -0,0 +1,52 @@
+package chapter2;
+
+public class Ex5 {
+	public static void main(String[] args) {
+		
+		int [] korArr = new int[4];
+		korArr[0] =84;
+		korArr[1] =94;
+		korArr[2] =24;
+		korArr[3] =54;
+		
+		korArr[01] =75;
+		System.out.println(korArr[1]);
+		
+		double [] avgArr = new double [4];
+		
+		avgArr[1] = 67.0;
+		avgArr[2] = 81.3;
+		avgArr[3] = 54.67;
+		avgArr[3] = 64.67;
+		
+		avgArr[3] = avgArr[2];
+		avgArr[2] = avgArr[1];
+		avgArr[1] = avgArr[0];
+		
+		avgArr[0] = 78.99;
+		
+		int [] numArr = {2,4,6,7};
+		int num =numArr[2];
+		System.out.println(num);
+		
+		char[] charArr = {'z','y'};
+		char Arr = charArr[0];
+		System.out.println(Arr);
+		
+		int[] arr1;
+		arr1 = new int[2];
+		System.out.println(arr1[0]);
+		System.out.println(arr1[1]);
+		int[] arr2 = new int[2];
+				
+		
+		
+		
+		
+		
+				
+				
+				
+	}
+
+}
